583e701813a7885d151381a2b9e7c6179aae470e
caubekimo <caubekimo@gmail.com>
2022-01-11 04:40:14 -0800

    Add reference

--- p77091041/MiniProject2/P77091041/client.c
+++ p77091041/MiniProject2/P77091041/client.c
@@ -95,3 +95,4 @@ int main(int argc, char const *argv[])
 
 // https://riptutorial.com/c/example/8274/get-lines-from-a-file-using-getline--
 // https://www.geeksforgeeks.org/socket-programming-cc/
+// http://zake7749.github.io/2015/03/17/SocketProgramming/
